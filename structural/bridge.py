d54ebf0751c4040490ccfe1e45500af9a4430b09
godkist <2000kbj@naver.com>
2022-07-29 13:44:25 +0900

    bridge pattern

---
+++ structural/bridge.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Switch:
+    def __init__(self, appliance: Appliance) -> None:
+        self.appliance = appliance
+
+    def turn_on(self) -> str:
+        return f'Switch: {self.appliance.run()}'
+
+
+class RemoteControl(Switch):
+    def turn_on(self) -> str:
+        return f'RemoteControl: {self.appliance.run()}'
+
+
+class Appliance(ABC):
+    @abstractmethod
+    def run(self) -> str:
+        pass
+
+
+class TV(Appliance):
+    def run(self) -> str:
+        return 'TV turned on'
+
+
+class VacuumCleaner(Appliance):
+    def run(self) -> str:
+        return 'VacuumCleaner turned on'
+
+
+def client_code(switch: Switch) -> None:
+    print(switch.turn_on())
+
+
+if __name__ == "__main__":
+    appliance = TV()
+    switch = RemoteControl(appliance)
+    client_code(switch)
+
+    appliance = VacuumCleaner()
+    switch = Switch(appliance)
+    client_code(switch)
